81014ecaf8f6a79fdcb89320a81f4fe52d4a9e1b
rantiputrinian <rantiputri@gmail.com>
2024-06-24 18:01:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li><router-link to="/todos">Todos</router-link></li>
+          <li><router-link to="/posts">Post</router-link></li>
+          <li><router-link to="/albums">Album Foto</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f7f7f7;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+header {
+  width: 100%;
+  background-color: #c59b12;
+  padding: 10px;
+}
+
+nav ul {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+}
+
+nav ul li {
+  margin: 0 15px;
+}
+
+nav ul li a {
+  color: rgb(165, 8, 8);
+  text-decoration: none;
+  font-weight: bold;
+}
+
+#app {
+  background-color: #f7e6ae;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 400px;
+}
+</style>
